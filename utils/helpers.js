3f8950088780245f66df2702e24121bb8d62c535
ML <labrecquemegan@hotmail.com>
2022-02-27 13:46:56 -0500

    added helpers for time and auth

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,11 @@
 module.exports = {
   format_date: date => {
     // How do we format the date and time?
+    return date.toLocaleTimeString();
+  },
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+      new Date(date).getFullYear() + 5
+    }`;
   }
 };
