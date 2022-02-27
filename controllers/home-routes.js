f5882c923bb94aa143098eb87029a8eaf172b3e2
ML <labrecquemegan@hotmail.com>
2022-02-27 12:51:30 -0500

    started routes, need to fix them

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,23 @@ const { Post, Comment, User } = require('../models');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
-  
+    try {
+        if (!req.session.logged_in) {
+          res.redirect('/login');
+          return;
+        }
+        // USER INFO
+        const userData = await User.findByPk(req.session.user_id, {
+          attributes: {exclude: ['password']},
+        });
+        const user = userData.get({plain: true});
+        res.render('all-posts', {
+            user,
+            logged_in: req.session.logged_in,
+          });
+        } catch (err) {
+          res.status(500).json(err);
+        }
 });
 
 // get single post
@@ -12,11 +28,15 @@ router.get('/post/:id', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-
-});
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+      }
+      res.render('login');
+    });
 
 router.get('/signup', (req, res) => {
- 
+    res.render('signup');
 });
 
 module.exports = router;
