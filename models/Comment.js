8dc7337c41d75241ac6124adf5358e528f41e4a2
ML <labrecquemegan@hotmail.com>
2022-03-04 21:16:42 -0500

    updated comment model with missing info

--- models/Comment.js
+++ models/Comment.js
@@ -7,13 +7,39 @@ class Comment extends Model {}
 Comment.init(
   // Fill in the missing data
   {
+    id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		text: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'users',
+				key: 'id',
+			},
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'posts',
+				key: 'id',
+			},
+		},
     body: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
   },
   {
-    sequelize
+    sequelize,
+    underscored: true,
+		modelName: 'comment',
   }
 );
 
