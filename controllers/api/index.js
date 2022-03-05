8663fd6c89a7a196653a34d51edde6c2a8f1c826
ML <labrecquemegan@hotmail.com>
2022-03-04 21:05:25 -0500

    updating api routes. So far have added in comment, index and pose routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 
 //Require the correct files into the api folder
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+const commentRoutes = require('./comment-routes');
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 
 //Create the router.use functionality for each required file
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes)
+
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,16 +1,72 @@
 const router = require('express').Router();
 //Require the correct files from the models and authorizations
+const withAuth = require('../../utils/auth');
+const { Post } = require('../../models');
+
+router.get('/', async (req, res) => {
+  // finds all posts
+	const postData = await Post.findAll();
+
+	res.status(200).json(postData);
+});
 
 router.post('/', withAuth, async (req, res) => {
 //Complete the asychronous function with error handling
+// create a post
+try {
+  const newPost = await Post.create({
+    ...req.body,
+    user_id: req.session.user_id,
+  });
+
+  res.status(200).json(newPost);
+} catch (err) {
+  res.status(400).json(err);
+}
 });
 
 router.put('/:id', withAuth, async (req, res) => {
 //Complete the asychronous function with error handling
+// updates a post
+try {
+  let updatedPost = await Post.update(req.body, {
+    where: {
+      id: req.params.id,
+      user_id: req.session.user_id,
+    },
+  });
+
+  if (!updatedPost) {
+    res.status(404).json({ message: 'No post found with that ID' });
+    return;
+  }
+
+  res.status(200).json(updatedPost);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.delete('/:id', withAuth, async (req, res) => {
   //Complete the asychronous function with error handling
+  // delete the post
+  try {
+		const postData = await Post.destroy({
+			where: {
+				id: req.params.id,
+				user_id: req.session.user_id,
+			},
+		});
+
+		if (!postData) {
+			res.status(404).json({ message: 'No post found with that ID' });
+			return;
+		}
+
+		res.status(200).json(postData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
