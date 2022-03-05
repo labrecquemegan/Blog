96bbd75b8b2b332194ee3efaf89fb2ae3d673994
ML <labrecquemegan@hotmail.com>
2022-03-04 23:22:31 -0500

    added stuff to server.js

--- server.js
+++ server.js
@@ -4,12 +4,34 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
 
 // Need a functionality do we need here?
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+	secret: 'Super secret secret',
+	cookie: {
+		maxAge: 86400,
+	},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 
