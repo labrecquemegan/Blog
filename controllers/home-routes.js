962d734e2b3582bc7492609729ffafd6775d7dbb
ML <labrecquemegan@hotmail.com>
2022-03-04 20:57:13 -0500

    updated index and home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,23 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    const dbPostData = await Post.findAll({
+    const postData = await Post.findAll({
       include: [
         {
-          model: Comment,
-          attributes: ['body'],
+          model: User,
         },
       ],
     });
 
-    const posts = dbPostData.map((post) =>
+    const posts = postData.map((post) =>
       post.get({ plain: true })
     );
 
-    res.render('homepage', {
+    res.render('all-posts', {
       posts,
       loggedIn: req.session.loggedIn,
     });
@@ -29,21 +29,25 @@ router.get('/', async (req, res) => {
 });
 
 // get single post
-router.get('/post/:id', withAuth, async (req, res) => {
+router.get('/post/:id', async (req, res) => {
   try {
-    const dbPostData = await Post.findByPk(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
       include: [
+        {model: User},
         {
           model: Comment,
-          attributes: [
-            'body'
-          ],
+          include: {model: User},
         },
       ],
     });
 
-    const post = dbPostData.get({ plain: true });
-    res.render('post', { post, loggedIn: req.session.loggedIn });
+    if (!postData) {
+			res.status(404).json({ message: 'No post by that ID' });
+		}
+		const comments = post.comments;
+    const post = postData.get({ plain: true });
+
+    res.render('single-post', { post, comments, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -51,16 +55,49 @@ router.get('/post/:id', withAuth, async (req, res) => {
 
 });
 
+// edit posts
+router.get('/post/edit/:id', withAuth, async (req, res) => {
+	try {
+		const postData = await Post.findByPk(req.params.id);
+		const post = postData.get({ plain: true });
+
+		res.render('edit-post', { post, logged_in: req.session.logged_in });
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// login
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-        res.redirect('/');
+        res.redirect('/dashboard');
         return;
       }
       res.render('login');
     });
 
+// signup
 router.get('/signup', (req, res) => {
     res.render('signup');
+});
+
+// dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
+	try {
+		const userData = await User.findByPk(req.session.user_id, {
+			attributes: { exclude: ['password'] },
+			include: [{ model: Post }],
+		});
+
+		const user = userData.get({ plain: true });
+
+		res.render('dashboard', {
+			...user,
+			logged_in: req.session.logged_in,
+		});
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
 
 //Create the api routes and require them to use them within the controllers
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
