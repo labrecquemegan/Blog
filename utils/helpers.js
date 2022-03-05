98227b33b6798f70031c2f54d904db72348b2cb7
ML <labrecquemegan@hotmail.com>
2022-03-04 22:42:21 -0500

    Added all-posts and edit-posts handlebars working from inclass examples

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,7 @@
 module.exports = {
-  format_date: date => {
-    // How do we format the date and time?
-    return date.toLocaleTimeString();
-  },
   format_date: (date) => {
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-      new Date(date).getFullYear() + 5
+      new Date(date).getFullYear()
     }`;
   }
 };
